b652ded1f01e35abe396c7d72715763f1e4bc464
Nelga <contact@nelga.fr>
2020-12-09 11:03:44 +0100

    cours
    09/12/2020

---
+++ php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test en PHP</title>
+</head>
+<body>
+    <h1>Test</h1>
+
+    <?php 
+        // Echo renvoi une chaine de caractère
+        echo '<h2>Test</h2>';
+
+
+    ?>
+
+</body>
+</html>
