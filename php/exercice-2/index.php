23133ca64ccd17253a8422c30916811168ca4808
Nelga <contact@nelga.fr>
2020-12-09 14:11:13 +0100

    Arold

--- php/exercice-2/index.php
+++ php/exercice-2/index.php
@@ -5,16 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercice 2</title>
 </head>
-<body>
-    <?php 
-        $name = 'Gérard';
-        $color = "#c42d2d";
-    ?>
-    <h1>Bienvenue <?php echo $name ?></h1>
-</body>
+
+<?php 
+    $name = 'Gérard';
+    $color = "#c42d2d";
+?>
+
 <style>
     body {
         background-color: <?php echo $color ?>; 
     }
 </style>
+
+<body>
+    <h1>Bienvenue <?php echo $name ?></h1>
+</body>
+
 </html>
