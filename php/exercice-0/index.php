8c23e50b7b1dfa63ceb41eacf876d3064d74a5c4
Nelga <contact@nelga.fr>
2020-12-09 11:44:46 +0100

    Update index.php

---
+++ php/exercice-0/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1</title>
+</head>
+<body>
+    <h1>Liste de fruits</h1>
+    <ul>
+        <li><?php echo "Fraise"?></li>
+        <li><?php echo "Banane"?></li>
+        <li><?php echo "Poire"?></li>
+        <li><?php echo "Abricot"?></li>
+        <li><?php echo "Arold"?></li>
+    </ul>
+</body>
+</html>
