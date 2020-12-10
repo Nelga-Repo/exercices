75ddf32ca5cee4b1136b9583528a3ed392776059
Nelga <contact@nelga.fr>
2020-12-10 10:30:09 +0100

    exercice 6

---
+++ php/exercice-6/index.php
@@ -0,0 +1,21 @@
+<?php
+    $prenom = ['Alice', 'Louis', 'Kevin', 'Roger', 'Remi', 'Léa', 'Justine', 'Philippe', 'Marion', 'Marie', 'Marine'];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 6</title>
+</head>
+<body>
+    <ul>
+        <?php
+            for ($i = 0; $i < 11; $i++) {
+                echo '<li>' . $prenom[$i] . '</li>';
+            }
+        ?>
+    </ul>
+</body>
+</html>
