72c03a49ec8b8ee5bc2e6f69c4af8012c77889f6
Nelga <contact@nelga.fr>
2020-12-11 15:47:10 +0100

    Create index.php

---
+++ php/exercice-10/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 10</title>
+</head>
+<body>
+    <?php
+        setlocale(LC_TIME, "fr_FR");
+        echo strftime('%A %d %B %G %H:%M:%S');
+    ?> 
+</body>
+</html>
