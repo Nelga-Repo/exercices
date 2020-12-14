c7eff1a2790c0f518ac81a0d9bc4098394a43b91
Nelga <contact@joumel.fr>
2020-12-14 11:03:00 +0100

    Menu

---
+++ php/exercice-11/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+  </head>
+  <body>
+    <main>
+      <h1>Accueil</h1>
+    </main>
+    <nav>
+      <ul>
+        <a href="index.php"><li>Accueil</li></a>
+        <a href="shop.php"><li>Shop</li></a>
+        <a href="contact.php"><li>Contact</li></a>
+      </ul>
+    </nav>
+  </body>
+</html>
