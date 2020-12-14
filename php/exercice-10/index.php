13d742c5a4e9518dd21e27e07994934e1e49d89c
Nelga <contact@joumel.fr>
2020-12-14 08:44:21 +0100

    Bon bon..

--- php/exercice-10/index.php
+++ php/exercice-10/index.php
@@ -11,6 +11,12 @@
       $date = new DateTime('2004-04-16 12:00:00');
       $date->add(new DateInterval('P435D'));
       echo $date->format('l d F Y G:i:s');
+      echo '<br>';
+    ?>
+    <?php
+      setlocale(LC_TIME, "fr_FR.utf8");
+      $Date = "2004-04-16 12:00:00";
+      echo date('l d F Y G:i:s', strtotime($Date. ' + 435 days'));
     ?>
 </body>
 </html>
